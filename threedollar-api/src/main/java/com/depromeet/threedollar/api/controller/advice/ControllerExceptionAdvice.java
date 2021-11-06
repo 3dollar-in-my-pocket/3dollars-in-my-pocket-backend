2bab34708b8f8fd97df92a596386c136e1f6ef10
seungh0 <will.seungho@gmail.com>
2021-11-07 06:48:57 +0900

    [TD-277] Request 에러에 대한 에러 메시지를 좀 더 상세하게 표현하도록 수정

--- threedollar-api/src/main/java/com/depromeet/threedollar/api/controller/advice/ControllerExceptionAdvice.java
+++ threedollar-api/src/main/java/com/depromeet/threedollar/api/controller/advice/ControllerExceptionAdvice.java
@@ -2,19 +2,23 @@ package com.depromeet.threedollar.api.controller.advice;
 
 import com.depromeet.threedollar.api.event.ServerErrorOccuredEvent;
 import com.depromeet.threedollar.application.common.dto.ApiResponse;
-import com.depromeet.threedollar.common.exception.*;
+import com.depromeet.threedollar.common.exception.ErrorCode;
 import com.depromeet.threedollar.common.exception.model.ThreeDollarsBaseException;
 import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.TypeMismatchException;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.BindException;
+import org.springframework.validation.FieldError;
 import org.springframework.web.HttpMediaTypeException;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
-import org.springframework.web.bind.*;
+import org.springframework.web.bind.MissingRequestValueException;
+import org.springframework.web.bind.ServletRequestBindingException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -34,20 +38,55 @@ public class ControllerExceptionAdvice {
 
     /**
      * 400 BadRequest
-     * 잘못된 입력이 들어왔을 경우 발생하는 Exception
+     * Spring Validation
      */
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(BindException.class)
     protected ApiResponse<Object> handleBadRequest(final BindException e) {
         log.error(e.getMessage());
-        return ApiResponse.error(VALIDATION_EXCEPTION, Objects.requireNonNull(e.getBindingResult().getFieldError()).getDefaultMessage());
+        FieldError fieldError = Objects.requireNonNull(e.getFieldError());
+        return ApiResponse.error(VALIDATION_EXCEPTION, String.format("%s (%s)", fieldError.getDefaultMessage(), fieldError.getField()));
     }
 
+    /**
+     * 400 BadRequest
+     * 잘못된 Enum 값이 입려된 경우 발생하는 Exception
+     */
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    protected ApiResponse<Object> handleHttpMessageNotReadableException(final HttpMessageNotReadableException e) {
+        log.error(e.getMessage());
+        return ApiResponse.error(VALIDATION_ENUM_VALUE_EXCEPTION);
+    }
+
+    /**
+     * 400 BadRequest
+     * RequestParam, RequestPath, RequestPart 등의 필드가 입력되지 않은 경우 발생하는 Exception
+     */
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MissingRequestValueException.class)
+    protected ApiResponse<Object> handle(final MissingRequestValueException e) {
+        log.error(e.getMessage());
+        return ApiResponse.error(VALIDATION_REQUEST_MISSING_EXCEPTION);
+    }
+
+    /**
+     * 400 BadRequest
+     * 잘못된 타입이 입력된 경우 발생하는 Exception
+     */
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(TypeMismatchException.class)
+    protected ApiResponse<Object> handleTypeMismatchException(final TypeMismatchException e) {
+        log.error(e.getMessage());
+        return ApiResponse.error(VALIDATION_WRONG_TYPE_EXCEPTION, String.format("%s (%s)", VALIDATION_WRONG_TYPE_EXCEPTION.getMessage(), e.getValue()));
+    }
+
+    /**
+     * 400 BadRequest
+     */
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler({
-        HttpMessageNotReadableException.class,
         InvalidFormatException.class,
-        MissingRequestValueException.class,
         ServletRequestBindingException.class
     })
     protected ApiResponse<Object> handleInvalidFormatException(final Exception e) {
@@ -64,6 +103,16 @@ public class ControllerExceptionAdvice {
     protected ApiResponse<Object> handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e) {
         log.error(e.getMessage());
         return ApiResponse.error(METHOD_NOT_ALLOWED_EXCEPTION);
+    }
+
+    /**
+     * 406 Not Acceptable
+     */
+    @ResponseStatus(HttpStatus.NOT_ACCEPTABLE)
+    @ExceptionHandler(HttpMediaTypeNotAcceptableException.class)
+    protected ApiResponse<Object> handleHttpMediaTypeNotAcceptableException(HttpMediaTypeNotAcceptableException e) {
+        log.error(e.getMessage());
+        return ApiResponse.error(NOT_ACCEPTABLE_EXCEPTION);
     }
 
     /**
--- threedollar-common/src/main/java/com/depromeet/threedollar/common/exception/ErrorCode.java
+++ threedollar-common/src/main/java/com/depromeet/threedollar/common/exception/ErrorCode.java
@@ -13,47 +13,53 @@ import static com.depromeet.threedollar.common.exception.ErrorStatusCode.BAD_REQ
 public enum ErrorCode {
 
     // 400 Bad Request
-    VALIDATION_EXCEPTION(BAD_REQUEST, OFF, "V000", "잘못된 요청입니다"),
-    VALIDATION_RATING_EXCEPTION(BAD_REQUEST, OFF, "V001", "허용되지 않은 평가 점수입니다. (0 ~ 5)"),
-    VALIDATION_LATITUDE_EXCEPTION(BAD_REQUEST, OFF, "V002", "허용되지 않은 위도 범위를 입력하였습니다. (33 ~ 43)"),
-    VALIDATION_LONGITUDE_EXCEPTION(BAD_REQUEST, OFF, "V003", "허용되지 않은 경도 범위를 입력하였습니다. (124 ~ 132)"),
-    VALIDATION_FILE_TYPE_EXCEPTION(BAD_REQUEST, OFF, "V004", "허용되지 않은 파일 형식입니다"),
-    VALIDATION_APPLE_TOKEN_EXCEPTION(BAD_REQUEST, OFF, "V005", "잘못된 애플 토큰입니다"),
-    VALIDATION_APPLE_TOKEN_EXPIRED_EXCEPTION(BAD_REQUEST, OFF, "V006", "만료된 애플 토큰입니다"),
-    VALIDATION_SOCIAL_TYPE_EXCEPTION(BAD_REQUEST, OFF, "V007", "잘못된 소셜 타입 입니다."),
-    VALIDATION_FILE_UPLOAD_EXCEPTION(BAD_REQUEST, OFF, "V008", "파일 업로드 중 에러가 발생하였습니다"),
+    VALIDATION_EXCEPTION(BAD_REQUEST, OFF, "BR001", "잘못된 요청입니다"),
+    VALIDATION_RATING_EXCEPTION(BAD_REQUEST, OFF, "BR002", "허용되지 않은 평가 점수입니다. (0 ~ 5)"),
+    VALIDATION_LATITUDE_EXCEPTION(BAD_REQUEST, OFF, "BR003", "허용되지 않은 위도 범위를 입력하였습니다. (33 ~ 43)"),
+    VALIDATION_LONGITUDE_EXCEPTION(BAD_REQUEST, OFF, "BR004", "허용되지 않은 경도 범위를 입력하였습니다. (124 ~ 132)"),
+    VALIDATION_FILE_TYPE_EXCEPTION(BAD_REQUEST, OFF, "BR005", "허용되지 않은 파일 형식입니다"),
+    VALIDATION_APPLE_TOKEN_EXCEPTION(BAD_REQUEST, OFF, "BR006", "잘못된 애플 토큰입니다"),
+    VALIDATION_APPLE_TOKEN_EXPIRED_EXCEPTION(BAD_REQUEST, OFF, "BR007", "만료된 애플 토큰입니다"),
+    VALIDATION_SOCIAL_TYPE_EXCEPTION(BAD_REQUEST, OFF, "BR008", "잘못된 소셜 타입 입니다."),
+    VALIDATION_FILE_UPLOAD_EXCEPTION(BAD_REQUEST, OFF, "BR009", "파일 업로드 중 에러가 발생하였습니다"),
+    VALIDATION_ENUM_VALUE_EXCEPTION(BAD_REQUEST, OFF, "BR010", "잘못된 Enum 값 입니다"),
+    VALIDATION_REQUEST_MISSING_EXCEPTION(BAD_REQUEST, OFF, "BR011", "필수적인 요청 값이 입력되지 않았습니다"),
+    VALIDATION_WRONG_TYPE_EXCEPTION(BAD_REQUEST, OFF, "BR012", "잘못된 타입이 입력되었습니다."),
 
     // 401 UnAuthorized
-    UNAUTHORIZED_EXCEPTION(UNAUTHORIZED, OFF, "C002", "세션이 만료되었습니다. 다시 로그인 해주세요"),
+    UNAUTHORIZED_EXCEPTION(UNAUTHORIZED, OFF, "UA001", "세션이 만료되었습니다. 다시 로그인 해주세요"),
 
     // 403 Forbidden
-    FORBIDDEN_EXCEPTION(FORBIDDEN, OFF, "C003", "허용하지 않는 요청입니다."),
+    FORBIDDEN_EXCEPTION(FORBIDDEN, OFF, "F001", "허용하지 않는 요청입니다."),
 
     // 404 Not Found
-    NOT_FOUND_EXCEPTION(NOT_FOUND, OFF, "C004", "존재하지 않습니다"),
-    NOT_FOUND_USER_EXCEPTION(NOT_FOUND, OFF, "N001", "존재하지 않는 유저입니다"),
-    NOT_FOUND_STORE_EXCEPTION(NOT_FOUND, OFF, "N002", "존재하지 않는 가게입니다"),
-    NOT_FOUND_REVIEW_EXCEPTION(NOT_FOUND, OFF, "N003", "존재하지 않는 리뷰입니다"),
-    NOT_FOUND_STORE_IMAGE_EXCEPTION(NOT_FOUND, OFF, "N003", "존재하지 않는 가게 이미지입니다"),
-    NOT_FOUND_FAQ_EXCEPTION(NOT_FOUND, OFF, "N004", "존재하지 않는 FAQ입니다"),
+    NOT_FOUND_EXCEPTION(NOT_FOUND, OFF, "NF001", "존재하지 않습니다"),
+    NOT_FOUND_USER_EXCEPTION(NOT_FOUND, OFF, "NF002", "존재하지 않는 유저입니다"),
+    NOT_FOUND_STORE_EXCEPTION(NOT_FOUND, OFF, "NF003", "존재하지 않는 가게입니다"),
+    NOT_FOUND_REVIEW_EXCEPTION(NOT_FOUND, OFF, "NF004", "존재하지 않는 리뷰입니다"),
+    NOT_FOUND_STORE_IMAGE_EXCEPTION(NOT_FOUND, OFF, "NF005", "존재하지 않는 가게 이미지입니다"),
+    NOT_FOUND_FAQ_EXCEPTION(NOT_FOUND, OFF, "NF006", "존재하지 않는 FAQ입니다"),
 
     // 405 Method Not Allowed
-    METHOD_NOT_ALLOWED_EXCEPTION(METHOD_NOT_ALLOWED, OFF, "C007", "지원하지 않는 메소드 입니다"),
+    METHOD_NOT_ALLOWED_EXCEPTION(METHOD_NOT_ALLOWED, OFF, "MN001", "지원하지 않는 메소드 입니다"),
+
+    // 406 Not Acceptable
+    NOT_ACCEPTABLE_EXCEPTION(NOT_ACCEPTABLE, OFF, "NA001", "Not Acceptable"),
 
     // 409 Conflict
-    CONFLICT_EXCEPTION(CONFLICT, OFF, "C005", "이미 존재합니다"),
+    CONFLICT_EXCEPTION(CONFLICT, OFF, "CF001", "이미 존재합니다"),
 
     // 415 Unsupported Media Type
-    UNSUPPORTED_MEDIA_TYPE_EXCEPTION(UNSUPPORTED_MEDIA_TYPE, OFF, "C010", "해당하는 미디어 타입을 지원하지 않습니다"),
+    UNSUPPORTED_MEDIA_TYPE_EXCEPTION(UNSUPPORTED_MEDIA_TYPE, OFF, "UM001", "해당하는 미디어 타입을 지원하지 않습니다"),
 
     // 400 Internal Server Exception
-    INTERNAL_SERVER_EXCEPTION(INTERNAL_SERVER, ON, "C006", "서버 내부에서 에러가 발생하였습니다"),
+    INTERNAL_SERVER_EXCEPTION(INTERNAL_SERVER, ON, "IS001", "서버 내부에서 에러가 발생하였습니다"),
 
     // 502 Bad Gateway
-    BAD_GATEWAY_EXCEPTION(BAD_GATEWAY, ON, "C008", "외부 연동 중 에러가 발생하였습니다"),
+    BAD_GATEWAY_EXCEPTION(BAD_GATEWAY, ON, "BG001", "외부 연동 중 에러가 발생하였습니다"),
 
     // 503 Service UnAvailable
-    SERVICE_UNAVAILABLE_EXCEPTION(SERVICE_UNAVAILABLE, OFF, "C009", "서비스를 이용하실 수 없습니다"),
+    SERVICE_UNAVAILABLE_EXCEPTION(SERVICE_UNAVAILABLE, OFF, "SU001", "서비스를 이용하실 수 없습니다"),
     ;
 
     private final ErrorStatusCode statusCode;
