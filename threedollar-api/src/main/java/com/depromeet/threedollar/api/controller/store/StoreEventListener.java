90ec0ca7c5bd23cb793c58fd468a3c110da00380
seungh0 <will.seungho@gmail.com>
2021-11-18 04:58:37 +0900

    [TD-306] 리뷰 등록/수정/삭제시 동기적으로 가게의 평균 점수를 계산하도록 변경
    - 리뷰가 DB에 반영되는 시기보다 평균 점수를 계산하는 시점이 빨라서 갱신이 안되는 경우가 존재하는 문제 발생

--- threedollar-api/src/main/java/com/depromeet/threedollar/api/controller/store/StoreEventListener.java
+++ threedollar-api/src/main/java/com/depromeet/threedollar/api/controller/store/StoreEventListener.java
@@ -4,7 +4,6 @@ import com.depromeet.threedollar.api.event.ReviewChangedEvent;
 import com.depromeet.threedollar.api.service.store.StoreRatingService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.event.EventListener;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 @RequiredArgsConstructor
@@ -13,7 +12,6 @@ public class StoreEventListener {
 
     private final StoreRatingService storeRatingService;
 
-    @Async
     @EventListener
     public void renewStoreRating(ReviewChangedEvent event) {
         storeRatingService.renewStoreRating(event.getStore());
