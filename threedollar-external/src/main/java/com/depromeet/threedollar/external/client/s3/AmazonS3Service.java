8972bd5c3fce960032ddfe3764cc2e5060790833
Seungho Kang <will.seungho@gmail.com>
2021-11-25 21:00:02 +0900

    [TD-330] S3에 저장한 이미지를 CloudFront를 통해 접근하도록 수정 (#50)

--- threedollar-external/src/main/java/com/depromeet/threedollar/external/client/s3/AmazonS3Service.java
+++ threedollar-external/src/main/java/com/depromeet/threedollar/external/client/s3/AmazonS3Service.java
@@ -4,6 +4,7 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.depromeet.threedollar.external.client.s3.dto.properties.AmazonCloudFrontProperties;
 import com.depromeet.threedollar.external.client.s3.dto.properties.AmazonS3Properties;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -15,17 +16,19 @@ import java.io.InputStream;
 public class AmazonS3Service implements S3Service {
 
     private final AmazonS3 amazonS3;
-    private final AmazonS3Properties component;
+
+    private final AmazonS3Properties s3Properties;
+    private final AmazonCloudFrontProperties cloudFrontProperties;
 
     @Override
     public void uploadFile(InputStream inputStream, ObjectMetadata objectMetadata, String fileName) {
-        amazonS3.putObject(new PutObjectRequest(component.getBucket(), fileName, inputStream, objectMetadata)
+        amazonS3.putObject(new PutObjectRequest(s3Properties.getBucket(), fileName, inputStream, objectMetadata)
             .withCannedAcl(CannedAccessControlList.PublicRead));
     }
 
     @Override
     public String getFileUrl(String fileName) {
-        return amazonS3.getUrl(component.getBucket(), fileName).toString();
+        return cloudFrontProperties.getFullPathFileUrl(fileName);
     }
 
 }
---
+++ threedollar-external/src/main/java/com/depromeet/threedollar/external/client/s3/dto/properties/AmazonCloudFrontProperties.java
@@ -0,0 +1,23 @@
+package com.depromeet.threedollar.external.client.s3.dto.properties;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
+
+@ToString
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+@ConstructorBinding
+@ConfigurationProperties("cloud.aws.cloudfront")
+public class AmazonCloudFrontProperties {
+
+    private final String url;
+
+    public String getFullPathFileUrl(String fileName) {
+        return this.url.concat(fileName);
+    }
+
+}
