7c6c43ec92ed551447e5c95dc0cc0a6586cb2550
seungh0 <will.seungho@gmail.com>
2021-11-28 21:58:08 +0900

    [TD-343] 유저의 활동이력 응답 DTO 폼 변경

---
+++ threedollar-api/src/main/java/com/depromeet/threedollar/api/service/user/dto/response/UserWithActivityResponse.kt
@@ -0,0 +1,36 @@
+package com.depromeet.threedollar.api.service.user.dto.response
+
+import com.depromeet.threedollar.domain.domain.medal.UserMedalType
+import com.depromeet.threedollar.domain.domain.user.User
+import com.depromeet.threedollar.domain.domain.user.UserSocialType
+
+data class UserActivityResponse(
+    val userId: Long,
+    val name: String,
+    val socialType: UserSocialType,
+    val medalType: UserMedalType?,
+    val activity: ActivityResponse
+) {
+
+    companion object {
+        fun of(
+            user: User,
+            reportedStoresCount: Long,
+            writtenReviewsCount: Long,
+            ownedMedalsCounts: Long
+        ): UserActivityResponse {
+            return UserActivityResponse(
+                user.id, user.name, user.socialType, user.medalType,
+                ActivityResponse(reportedStoresCount, writtenReviewsCount, ownedMedalsCounts)
+            )
+        }
+    }
+
+}
+
+
+data class ActivityResponse(
+    val reportedStoresCount: Long,
+    val writtenReviewsCount: Long,
+    val ownedMedalsCounts: Long
+)
