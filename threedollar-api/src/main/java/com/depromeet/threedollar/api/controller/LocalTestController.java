00833c05291f299ad8315fc44f01cf2133988468
seungh0 <will.seungho@gmail.com>
2021-12-02 02:05:55 +0900

    [TD-358] 개발 서버용 가게 강제 삭제 API 추가

--- threedollar-api/src/main/java/com/depromeet/threedollar/api/controller/LocalTestController.java
+++ threedollar-api/src/main/java/com/depromeet/threedollar/api/controller/LocalTestController.java
@@ -1,6 +1,9 @@
 package com.depromeet.threedollar.api.controller;
 
+import com.depromeet.threedollar.api.service.store.StoreServiceUtils;
 import com.depromeet.threedollar.application.common.dto.ApiResponse;
+import com.depromeet.threedollar.domain.domain.store.Store;
+import com.depromeet.threedollar.domain.domain.store.StoreRepository;
 import com.depromeet.threedollar.domain.domain.user.User;
 import com.depromeet.threedollar.domain.domain.user.UserRepository;
 import com.depromeet.threedollar.domain.domain.user.UserSocialType;
@@ -8,7 +11,9 @@ import com.depromeet.threedollar.api.service.auth.dto.response.LoginResponse;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Profile;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
@@ -24,6 +29,7 @@ public class LocalTestController {
 
     private final HttpSession httpSession;
     private final UserRepository userRepository;
+    private final StoreRepository storeRepository;
 
     @ApiOperation("[개발 서버용] 테스트를 위한 토큰을 받아옵니다")
     @GetMapping("/test-token")
@@ -34,6 +40,15 @@ public class LocalTestController {
         }
         httpSession.setAttribute(USER_ID, user.getId());
         return ApiResponse.success(LoginResponse.of(httpSession.getId(), user.getId()));
+    }
+
+    @ApiOperation("[개발 서버용] 가게를 강제 삭제 합니다")
+    @DeleteMapping("/test-delete-store")
+    public ApiResponse<String> deleteStoreByForce(@RequestParam Long storeId) {
+        Store store = StoreServiceUtils.findStoreById(storeRepository, storeId);
+        store.delete();
+        storeRepository.save(store);
+        return ApiResponse.SUCCESS;
     }
 
 }
