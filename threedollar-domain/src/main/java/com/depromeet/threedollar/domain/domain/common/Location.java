51a4666b4cde89fc5c4ea7f20425e20388353cda
seungh0 <will.seungho@gmail.com>
2021-12-06 13:51:16 +0900

    [TD-368] 위도 경도 예외 메시지 수정

--- threedollar-domain/src/main/java/com/depromeet/threedollar/domain/domain/common/Location.java
+++ threedollar-domain/src/main/java/com/depromeet/threedollar/domain/domain/common/Location.java
@@ -40,10 +40,10 @@ public class Location {
 
     private void validateIsScopeOfKorea(double latitude, double longitude) {
         if (latitude < SOUTH_KOREA_MIN_LATITUDE || latitude > SOUTH_KOREA_MAX_LATITUDE) {
-            throw new ValidationException(String.format("잘못된 위도 (%s)가 입력되었습니다. (33.1 ~ 38.61) 사이의 범위만 허용됩니다)", latitude), VALIDATION_LATITUDE_EXCEPTION);
+            throw new ValidationException(String.format("잘못된 위도 (%s)가 입력되었습니다. (%s ~ %s) 사이의 위도만 허용됩니다)", latitude, SOUTH_KOREA_MIN_LATITUDE, SOUTH_KOREA_MAX_LATITUDE), VALIDATION_LATITUDE_EXCEPTION);
         }
         if (longitude < SOUTH_KOREA_MIN_LONGITUDE || longitude > SOUTH_KOREA_MAX_LONGITUDE) {
-            throw new ValidationException(String.format("잘못된 경도 (%s)가 입력되었습니다. (124.6 ~ 131.87) 사이의 범위만 허용됩니다)", longitude), VALIDATION_LONGITUDE_EXCEPTION);
+            throw new ValidationException(String.format("잘못된 경도 (%s)가 입력되었습니다. (%s ~ %s) 사이의 경도만 허용됩니다)", longitude, SOUTH_KOREA_MIN_LONGITUDE, SOUTH_KOREA_MAX_LONGITUDE), VALIDATION_LONGITUDE_EXCEPTION);
         }
     }
 
