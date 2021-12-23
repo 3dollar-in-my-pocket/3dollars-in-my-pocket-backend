60f268cb278b01e3b6aa02e2c8f463fa698e0b09
seungh0 <will.seungho@gmail.com>
2021-12-23 13:26:56 +0900

    [TD-405] 변수 네이밍 coordinate -> geoCoordinate로 수정

--- threedollar-api/src/main/java/com/depromeet/threedollar/api/controller/store/StoreRetrieveController.java
+++ threedollar-api/src/main/java/com/depromeet/threedollar/api/controller/store/StoreRetrieveController.java
@@ -31,16 +31,16 @@ public class StoreRetrieveController {
     @ApiOperation(value = "메인 페이지 - 위도, 경도 기준 내 주위의 가게 목록을 조회합니다", notes = "orderType: 정렬이 필요한 경우 category: 카테고리 필터링이 필요한 경우")
     @GetMapping("/api/v2/stores/near")
     public ApiResponse<List<StoreWithVisitsAndDistanceResponse>> getNearStores(@Valid RetrieveNearStoresRequest request,
-                                                                               @GeoCoordinate Coordinate coordinate,
+                                                                               @GeoCoordinate Coordinate geoCoordinate,
                                                                                @MapCoordinate Coordinate mapCoordinate) {
-        return ApiResponse.success(storeRetrieveService.getNearStores(request, coordinate, mapCoordinate));
+        return ApiResponse.success(storeRetrieveService.getNearStores(request, geoCoordinate, mapCoordinate));
     }
 
     @ApiOperation("가게 상세 페이지 - 특정 가게의 정보를 상세 조회합니다")
     @GetMapping("/api/v2/store")
     public ApiResponse<StoreDetailResponse> getDetailStoreInfo(@Valid RetrieveStoreDetailRequest request,
-                                                               @GeoCoordinate Coordinate coordinate) {
-        return ApiResponse.success(storeRetrieveService.getDetailStoreInfo(request, coordinate));
+                                                               @GeoCoordinate Coordinate geoCoordinate) {
+        return ApiResponse.success(storeRetrieveService.getDetailStoreInfo(request, geoCoordinate));
     }
 
     @ApiOperation("[인증] 마이페이지 - 내가 제보한 가게 목록들을 스크롤 페이지네이션으로 조회합니다 (삭제된 가게 포함 O)")
@@ -60,9 +60,9 @@ public class StoreRetrieveController {
     @Auth
     @GetMapping("/api/v2/stores/me")
     public ApiResponse<StoresCursorV2Response> retrieveMyReportedStoreHistoriesV2(@Valid RetrieveMyStoresV2Request request,
-                                                                                  @GeoCoordinate(required = false) Coordinate coordinate,
+                                                                                  @GeoCoordinate(required = false) Coordinate geoCoordinate,
                                                                                   @UserId Long userId) {
-        return ApiResponse.success(storeRetrieveService.retrieveMyReportedStoreHistoriesV2(request, coordinate, userId));
+        return ApiResponse.success(storeRetrieveService.retrieveMyReportedStoreHistoriesV2(request, geoCoordinate, userId));
     }
 
     /**
@@ -73,9 +73,9 @@ public class StoreRetrieveController {
     @ApiOperation("[Deprecated] 가게 카테고리별 조회 페이지 - 거리순으로 특정 메뉴를 판매하는 가게 목록을 조회합니다")
     @GetMapping("/api/v2/stores/distance")
     public ApiResponse<StoresGroupByDistanceV2Response> getStoresGroupByDistance(@Valid RetrieveStoreGroupByCategoryV2Request request,
-                                                                                 @GeoCoordinate Coordinate coordinate,
+                                                                                 @GeoCoordinate Coordinate geoCoordinate,
                                                                                  @MapCoordinate Coordinate mapCoordinate) {
-        return ApiResponse.success(storeRetrieveService.getNearStoresGroupByDistance(request, coordinate, mapCoordinate));
+        return ApiResponse.success(storeRetrieveService.getNearStoresGroupByDistance(request, geoCoordinate, mapCoordinate));
     }
 
     /**
