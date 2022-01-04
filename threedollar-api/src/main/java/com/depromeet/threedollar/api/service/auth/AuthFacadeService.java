f5822194780fcce6cd7de37b3198c5d971d6c284
seungho <will.seungho@gmail.com>
2022-01-04 18:52:33 +0900

    [TD-419] AuthFascadeService 리팩토링

--- threedollar-api/src/main/java/com/depromeet/threedollar/api/service/auth/AuthFacadeService.java
+++ threedollar-api/src/main/java/com/depromeet/threedollar/api/service/auth/AuthFacadeService.java
@@ -2,45 +2,47 @@ package com.depromeet.threedollar.api.service.auth;
 
 import com.depromeet.threedollar.api.service.auth.dto.request.LoginRequest;
 import com.depromeet.threedollar.api.service.auth.dto.request.SignUpRequest;
-import com.depromeet.threedollar.common.exception.model.ValidationException;
+import com.depromeet.threedollar.common.exception.model.InternalServerException;
 import com.depromeet.threedollar.domain.domain.user.UserSocialType;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.stereotype.Component;
 
-import static com.depromeet.threedollar.common.exception.ErrorCode.VALIDATION_SOCIAL_TYPE_EXCEPTION;
+import java.util.Map;
 
 @RequiredArgsConstructor
 @Component
-public class AuthFacadeService {
+public class AuthFacadeService implements InitializingBean {
+
+    private final Map<UserSocialType, AuthService> authServiceMap;
 
     private final AuthService appleAuthService;
     private final AuthService kaKaoAuthService;
     private final AuthService googleAuthService;
 
+    @Override
+    public void afterPropertiesSet() {
+        authServiceMap.put(UserSocialType.APPLE, appleAuthService);
+        authServiceMap.put(UserSocialType.GOOGLE, googleAuthService);
+        authServiceMap.put(UserSocialType.KAKAO, kaKaoAuthService);
+    }
+
     public Long signUp(SignUpRequest request) {
-        if (UserSocialType.KAKAO.equals(request.getSocialType())) {
-            return kaKaoAuthService.signUp(request);
-        }
-        if (UserSocialType.APPLE.equals(request.getSocialType())) {
-            return appleAuthService.signUp(request);
-        }
-        if (UserSocialType.GOOGLE.equals(request.getSocialType())) {
-            return googleAuthService.signUp(request);
-        }
-        throw new ValidationException(String.format("허용하지 않는 소셜 타입 (%s) 입니다.", request.getSocialType()), VALIDATION_SOCIAL_TYPE_EXCEPTION);
+        AuthService authService = getAuthServiceBySocialType(request.getSocialType());
+        return authService.signUp(request);
     }
 
     public Long login(LoginRequest request) {
-        if (UserSocialType.KAKAO.equals(request.getSocialType())) {
-            return kaKaoAuthService.login(request);
+        AuthService authService = getAuthServiceBySocialType(request.getSocialType());
+        return authService.login(request);
+    }
+
+    private AuthService getAuthServiceBySocialType(UserSocialType socialType) {
+        AuthService authService = authServiceMap.get(socialType);
+        if (authService == null) {
+            throw new InternalServerException(String.format("해당 소셜 타입(%s)의 AuthService를 등록해주세요.", socialType));
         }
-        if (UserSocialType.APPLE.equals(request.getSocialType())) {
-            return appleAuthService.login(request);
-        }
-        if (UserSocialType.GOOGLE.equals(request.getSocialType())) {
-            return googleAuthService.login(request);
-        }
-        throw new ValidationException(String.format("허용하지 않는 소셜 타입 (%s) 입니다.", request.getSocialType()), VALIDATION_SOCIAL_TYPE_EXCEPTION);
+        return authService;
     }
 
 }
