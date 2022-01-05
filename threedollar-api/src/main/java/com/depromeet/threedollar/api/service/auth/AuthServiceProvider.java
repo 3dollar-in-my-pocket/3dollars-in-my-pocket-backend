c99ecc07289dd7552f4b0fd4b06053c2643d91c9
seungh0 <will.seungho@gmail.com>
2022-01-05 22:14:43 +0900

    [TD-424] AuthServiceProvider afterPropertiesSet() 대신 PostConstructor 사용

--- threedollar-api/src/main/java/com/depromeet/threedollar/api/service/auth/AuthServiceProvider.java
+++ threedollar-api/src/main/java/com/depromeet/threedollar/api/service/auth/AuthServiceProvider.java
@@ -6,30 +6,31 @@ import com.depromeet.threedollar.api.service.auth.policy.KaKaoAuthService;
 import com.depromeet.threedollar.common.exception.model.InternalServerException;
 import com.depromeet.threedollar.domain.domain.user.UserSocialType;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.InitializingBean;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import java.util.HashMap;
 import java.util.Map;
 
 @RequiredArgsConstructor
 @Component
-public class AuthServiceProvider implements InitializingBean {
+public class AuthServiceProvider {
 
-    private final Map<UserSocialType, AuthService> authServiceMap;
+    private static final Map<UserSocialType, AuthService> authServiceMap = new HashMap<>();
 
     private final AppleAuthService appleAuthService;
     private final KaKaoAuthService kaKaoAuthService;
     private final GoogleAuthService googleAuthService;
 
-    @Override
-    public void afterPropertiesSet() {
+    @PostConstruct
+    void initializeAuthServicesMap() {
         authServiceMap.put(UserSocialType.APPLE, appleAuthService);
-        authServiceMap.put(UserSocialType.GOOGLE, googleAuthService);
         authServiceMap.put(UserSocialType.KAKAO, kaKaoAuthService);
-        validateInitalizeAuthServices();
+        authServiceMap.put(UserSocialType.GOOGLE, googleAuthService);
+        validateInitializeAuthServices();
     }
 
-    private void validateInitalizeAuthServices() {
+    private void validateInitializeAuthServices() {
         for (UserSocialType userSocialType : UserSocialType.values()) {
             if (authServiceMap.get(userSocialType) == null) {
                 throw new InternalServerException(String.format("모든 소셜에 대한 AuthService가 등록되지 않았습니다. 해당 소셜 타입(%s)의 AuthService를 등록해주세요.", userSocialType));
