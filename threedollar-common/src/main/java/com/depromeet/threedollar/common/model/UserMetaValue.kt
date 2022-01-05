eba27bb7c88dc3fd5f73530088640c482dabc10b
seungh0 <will.seungho@gmail.com>
2022-01-05 20:43:04 +0900

    [TD-423] PlatformType -> OSPlatformType으로 네이밍 수정

--- threedollar-common/src/main/java/com/depromeet/threedollar/common/model/UserMetaValue.kt
+++ threedollar-common/src/main/java/com/depromeet/threedollar/common/model/UserMetaValue.kt
@@ -1,9 +1,9 @@
 package com.depromeet.threedollar.common.model
 
-import com.depromeet.threedollar.common.type.PlatformType
+import com.depromeet.threedollar.common.type.OsPlatformType
 
 data class UserMetaValue(
-    val platformType: PlatformType,
+    val osPlatform: OsPlatformType,
     val userAgent: String,
     val sourceIp: String,
 )
