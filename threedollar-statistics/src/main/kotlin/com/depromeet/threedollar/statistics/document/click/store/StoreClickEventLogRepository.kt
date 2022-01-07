d991d6d5d31ac20da127fe8f88ad95d0fcef1f5c
Seungho Kang <will.seungho@gmail.com>
2022-01-08 01:27:08 +0900

    [TD-427] 가슴속 삼천원 통계 모듈 추가 (#75)
    * [TD-427] consumer 모듈 추가 및 클릭 로그 이벤트 관리를 위한 구성
    
    * [TD-427] threedollar-consumer -> threedollar-statistics로 변경
    
    * [TD-427] 일단 개발 배포에는 ElasticSearch 포함하지 않도록 수정
    
    * [TD-427] 일단 개발 배포에는 ElasticSearch 포함하지 않도록 수정

---
+++ threedollar-statistics/src/main/kotlin/com/depromeet/threedollar/statistics/document/click/store/StoreClickEventLogRepository.kt
@@ -0,0 +1,11 @@
+package com.depromeet.threedollar.statistics.document.click.store
+
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository
+
+interface StoreClickEventLogRepository : ElasticsearchRepository<StoreClickEventLog, String> {
+
+    fun countByStoreId(storeId: Long): Long
+
+    fun countByStoreIdAndTimestampGreaterThanEqual(storeId: Long, timestamp: Long): Long
+
+}
