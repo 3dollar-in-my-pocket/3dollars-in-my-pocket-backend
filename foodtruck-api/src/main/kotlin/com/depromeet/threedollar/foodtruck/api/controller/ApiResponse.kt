91c1000b3b0d08fa1f642cec2e19964ffda48afe
seungh0 <will.seungho@gmail.com>
2022-01-11 19:24:35 +0900

    [TD-437] 사장님 모듈 ExceptionAdvice 추가

--- foodtruck-api/src/main/kotlin/com/depromeet/threedollar/foodtruck/api/controller/ApiResponse.kt
+++ foodtruck-api/src/main/kotlin/com/depromeet/threedollar/foodtruck/api/controller/ApiResponse.kt
@@ -1,5 +1,7 @@
 package com.depromeet.threedollar.foodtruck.api.controller
 
+import com.depromeet.threedollar.common.exception.type.ErrorCode
+
 data class ApiResponse<T>(
     val code: String,
     val message: String,
@@ -11,6 +13,14 @@ data class ApiResponse<T>(
 
         fun <T> success(data: T): ApiResponse<T> {
             return ApiResponse("", "", data)
+        }
+
+        fun error(errorCode: ErrorCode): ApiResponse<Nothing> {
+            return ApiResponse(errorCode.code, errorCode.message, null)
+        }
+
+        fun error(errorCode: ErrorCode, message: String): ApiResponse<Nothing> {
+            return ApiResponse(errorCode.code, message, null)
         }
     }
 
