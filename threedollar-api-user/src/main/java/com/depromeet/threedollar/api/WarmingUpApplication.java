4bf184adc04a162341f4497aff195ce85bb79b93
seungh0 <will.seungho@gmail.com>
2022-01-12 18:05:32 +0900

    [TD-447] Stage 환경 추가

--- threedollar-api-user/src/main/java/com/depromeet/threedollar/api/WarmingUpApplication.java
+++ threedollar-api-user/src/main/java/com/depromeet/threedollar/api/WarmingUpApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
-@Profile({"dev", "prod"})
+@Profile({"dev", "stage", "prod"})
 @RequiredArgsConstructor
 @Component
 public class WarmingUpApplication implements CommandLineRunner {
