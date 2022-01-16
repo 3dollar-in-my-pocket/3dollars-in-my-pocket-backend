28cbb9334248b94484f4665335376bd7783ecb5b
seungh0 <will.seungho@gmail.com>
2022-01-16 14:53:23 +0900

    [TD-461] 테스트 컨텍스트 재구성

--- threedollar-domain/src/test/java/com/depromeet/threedollar/domain/ThreeDollarApplicationTests.java
+++ threedollar-domain/src/test/java/com/depromeet/threedollar/domain/ThreeDollarApplicationTests.java
@@ -1,6 +1,5 @@
-package com.depromeet.threedollar;
+package com.depromeet.threedollar.domain;
 
-import com.depromeet.threedollar.domain.ThreeDollarDomainRoot;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
