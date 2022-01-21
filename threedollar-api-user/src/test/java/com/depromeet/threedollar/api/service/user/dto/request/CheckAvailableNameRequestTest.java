8f5499f201b12f7cfe3d3779a1613154aabed567
Seungho Kang <will.seungho@gmail.com>
2022-01-21 20:01:32 +0900

    [TD-480] 통합테스트와 단위 테스트 환경 분리 (#86)
    * [TD-480] Gradle 단위테스트와 통합테스트를 분리해서 테스트할 수 있도록 설정 (user-api, core-api, admin, domain, common 모듈 완료)
    
    * [TD-480] Gradle 단위테스트와 통합테스트를 분리해서 테스트할 수 있도록 설정 admin 모듈
    
    * [TD-480] 사장님 API 및 document 모듈 통합테스트 및 단위테스트 분리
    
    * [TD-480] Dockerfile 재정의
    
    * [TD-480] CI 통합테스트 및 단위테스트를 수행하도록 수정
    
    * [TD-480] jacoco 수정

--- threedollar-api-user/src/test/java/com/depromeet/threedollar/api/service/user/dto/request/CheckAvailableNameRequestTest.java
+++ threedollar-api-user/src/test/java/com/depromeet/threedollar/api/service/user/dto/request/CheckAvailableNameRequestTest.java
@@ -1,6 +1,5 @@
-package com.depromeet.threedollar.api.service.user.request;
+package com.depromeet.threedollar.api.service.user.dto.request;
 
-import com.depromeet.threedollar.api.service.user.dto.request.CheckAvailableNameRequest;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
