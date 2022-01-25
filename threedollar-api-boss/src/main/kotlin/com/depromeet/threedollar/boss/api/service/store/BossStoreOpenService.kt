afe1d6e3b4654e8808d74a59b8bb7a36dc9a4ab2
seungh0 <will.seungho@gmail.com>
2022-01-25 20:26:25 +0900

    [TD-506] 사장님 가게 오픈 갱신 API 추 (TTL 30분)

---
+++ threedollar-api-boss/src/main/kotlin/com/depromeet/threedollar/boss/api/service/store/BossStoreOpenService.kt
@@ -0,0 +1,25 @@
+package com.depromeet.threedollar.boss.api.service.store
+
+import com.depromeet.threedollar.document.boss.document.store.BossStoreRepository
+import com.depromeet.threedollar.redis.boss.domain.store.BossStoreOpenInfo
+import com.depromeet.threedollar.redis.boss.domain.store.BossStoreOpenInfoRepository
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Service
+import java.time.LocalDateTime
+
+@Service
+class BossStoreOpenService(
+    private val bossStoreOpenInfoRepository: BossStoreOpenInfoRepository,
+    private val bossStoreRepository: BossStoreRepository
+) {
+
+    fun renewBossStoreOpen(
+        bossStoreId: String
+    ) {
+        validateExistsBossStore(bossStoreRepository, bossStoreId)
+        val bossStoreOpenInfo = bossStoreOpenInfoRepository.findByIdOrNull(bossStoreId)
+            ?: BossStoreOpenInfo.of(bossStoreId, LocalDateTime.now())
+        bossStoreOpenInfoRepository.save(bossStoreOpenInfo);
+    }
+
+}
