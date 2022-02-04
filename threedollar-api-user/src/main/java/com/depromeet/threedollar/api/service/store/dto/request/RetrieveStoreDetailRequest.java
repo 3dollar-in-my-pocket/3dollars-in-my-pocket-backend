775eb63805f8e37f0aad0ec3f3cbd78d1181bb1f
seungh0 <will.seungho@gmail.com>
2022-02-04 19:51:10 +0900

    [TD-546] 가개 방문 기록 조회시 시작 범위가 미래인경우 Validation 에러

--- threedollar-api-user/src/main/java/com/depromeet/threedollar/api/service/store/dto/request/RetrieveStoreDetailRequest.java
+++ threedollar-api-user/src/main/java/com/depromeet/threedollar/api/service/store/dto/request/RetrieveStoreDetailRequest.java
@@ -4,6 +4,7 @@ import lombok.*;
 import org.jetbrains.annotations.Nullable;
 
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PastOrPresent;
 import java.time.LocalDate;
 
 @ToString
@@ -15,6 +16,7 @@ public class RetrieveStoreDetailRequest {
     @NotNull(message = "{store.storeId.notNull}")
     private Long storeId;
 
+    @PastOrPresent(message = "{visit.startDate.pastOrPresent}")
     @Nullable
     private LocalDate startDate;
 
