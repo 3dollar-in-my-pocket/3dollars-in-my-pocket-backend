6048e67d2c37277b505a4746561ead4534fc9b44
seungh0 <will.seungho@gmail.com>
2022-02-20 15:22:13 +0900

    [TD-584] 사장님 서버 서버 에러 발생시 슬랙에 알림을 주도록 설정 추가

--- threedollar-api-boss/src/main/kotlin/com/depromeet/threedollar/boss/api/listener/slack/SlackNotificationEventListener.kt
+++ threedollar-api-boss/src/main/kotlin/com/depromeet/threedollar/boss/api/listener/slack/SlackNotificationEventListener.kt
@@ -1,8 +1,10 @@
 package com.depromeet.threedollar.boss.api.listener.slack
 
 import com.depromeet.threedollar.document.boss.event.registration.NewBossAppliedRegistrationEvent
+import com.depromeet.threedollar.domain.common.event.ServerExceptionOccurredEvent
 import com.depromeet.threedollar.external.client.slack.SlackWebhookApiClient
 import com.depromeet.threedollar.external.client.slack.dto.request.PostSlackMessageRequest
+import com.depromeet.threedollar.external.client.slack.type.SlackNotificationMessageType
 import com.depromeet.threedollar.external.client.slack.type.SlackNotificationMessageType.NEW_BOSS_REGISTRATION_MESSAGE
 import org.springframework.context.event.EventListener
 import org.springframework.scheduling.annotation.Async
@@ -22,6 +24,23 @@ class SlackNotificationEventListener(
                     event.registration.boss.name,
                     event.registration.store.name,
                     event.registration.store.contactsNumber
+                )
+            )
+        )
+    }
+
+    @Async
+    @EventListener
+    fun sendErrorNotification(event: ServerExceptionOccurredEvent) {
+        slackNotificationApiClient.postMessage(
+            PostSlackMessageRequest.of(
+                SlackNotificationMessageType.ERROR_MESSAGE.generateMessage(
+                    event.applicationType.description,
+                    event.errorCode.code,
+                    event.timeStamp,
+                    event.userMetaValue,
+                    event.errorCode.message,
+                    event.exception
                 )
             )
         )
