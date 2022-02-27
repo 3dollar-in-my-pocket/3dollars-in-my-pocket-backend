058b14d4ce623fd0bb8806edad58c282a038d8ea
seungh0 <will.seungho@gmail.com>
2022-02-28 02:08:38 +0900

    [TD-603] 관리자 메달 요청 사용자 에러 메시지 프로퍼티 추가

--- threedollar-admin/src/main/kotlin/com/depromeet/threedollar/admin/service/medal/dto/request/AddMedalRequest.kt
+++ threedollar-admin/src/main/kotlin/com/depromeet/threedollar/admin/service/medal/dto/request/AddMedalRequest.kt
@@ -7,26 +7,26 @@ import org.hibernate.validator.constraints.URL
 import javax.validation.constraints.Min
 
 data class AddMedalRequest(
-    @field:Length(max = 30)
+    @field:Length(max = 30, message = "{medal.name.length}")
     val name: String,
 
-    @field:Length(max = 200)
+    @field:Length(max = 200, message = "{medal.introduction.length}")
     val introduction: String?,
 
-    @field:Length(max = 2048)
-    @field:URL
+    @field:Length(max = 2048, message = "{medal.activationIconUrl.length}")
+    @field:URL(message = "{medal.activationIconUrl.url}")
     val activationIconUrl: String,
 
-    @field:Length(max = 2048)
-    @field:URL
+    @field:Length(max = 2048, message = "{medal.disableIconUrl.length}")
+    @field:URL(message = "{medal.disableIconUrl.url}")
     val disableIconUrl: String,
 
     val conditionType: MedalAcquisitionConditionType,
 
-    @field:Min(value = 0)
+    @field:Min(value = 0, message = "{medal.conditionCount.min}")
     val conditionCount: Int,
 
-    @field:Length(max = 200)
+    @field:Length(max = 200, message = "{medal.acquisitionDescription.length}")
     val acquisitionDescription: String?
 ) {
 
