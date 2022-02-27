8bfa89e4b8954463c25f0b823e84bc843fc63fd0
seungh0 <will.seungho@gmail.com>
2022-02-27 14:50:41 +0900

    [TD-596] 테스트 케이스 추가

--- threedollar-api-user/src/integrationTest/java/com/depromeet/threedollar/api/controller/advertisement/AdvertisementControllerTest.kt
+++ threedollar-api-user/src/integrationTest/java/com/depromeet/threedollar/api/controller/advertisement/AdvertisementControllerTest.kt
@@ -27,6 +27,42 @@ class AdvertisementControllerTest(
         advertisementRepository.deleteAllInBatch()
     }
 
+    @DisplayName("GET /api/v1/popups")
+    @Test
+    fun `(DEPRECATED) 플랫폼에 해당하는 활성화중인 광고 목록을 조회한다`() {
+        // given
+        val platform = AdvertisementPlatformType.AOS
+        val advertisement = AdvertisementCreator.create(
+            AdvertisementPositionType.SPLASH,
+            platform,
+            "광고 타이틀",
+            "광고 내용\n광고 내용",
+            "https://pop-up-image.png",
+            "https://my-link.com",
+            "#ffffff",
+            "#000000",
+            LocalDateTime.of(2021, 1, 1, 0, 0),
+            LocalDateTime.now().plusDays(1),
+        )
+        advertisementRepository.save(advertisement)
+
+        // when & then
+        mockMvc.get("/v1/advertisements") {
+            param("platform", platform.toString())
+        }.andExpect {
+            status { isOk() }
+            content {
+                jsonPath("$.data[0].title") { value(advertisement.detail.title) }
+                jsonPath("$.data[0].subTitle") { value(advertisement.detail.subTitle) }
+                jsonPath("$.data[0].imageUrl") { value(advertisement.detail.imageUrl) }
+                jsonPath("$.data[0].linkUrl") { value(advertisement.detail.linkUrl) }
+                jsonPath("$.data[0].bgColor") { value(advertisement.detail.bgColor) }
+                jsonPath("$.data[0].fontColor") { value(advertisement.detail.fontColor) }
+            }
+        }
+
+    }
+
     @DisplayName("GET /api/v1/advertisements")
     @Test
     fun `플랫폼에 해당하는 활성화중인 광고 목록을 조회한다`() {
