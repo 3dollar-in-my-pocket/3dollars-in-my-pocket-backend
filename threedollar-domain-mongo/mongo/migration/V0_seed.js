e589a16399023137edbd25af592d5fe592ab39c9
seungh0 <will.seungho@gmail.com>
2022-03-12 02:06:32 +0900

    [TD-653] MongoDB 시드 데이터 관리 및 http 데이터 정리

---
+++ threedollar-domain-mongo/mongo/migration/V0_seed.js
@@ -0,0 +1,69 @@
+// boss_store_category_v1
+db.boss_store_category_v1.insertMany([
+    {
+        "name": "한식",
+        "sequencePriority": 10,
+        "createdAt": new Date(),
+        "updatedAt": new Date()
+    },
+    {
+        "name": "양식",
+        "sequencePriority": 20,
+        "createdAt": new Date(),
+        "updatedAt": new Date()
+    },
+    {
+        "name": "일식",
+        "sequencePriority": 30,
+        "createdAt": new Date(),
+        "updatedAt": new Date()
+    },
+    {
+        "name": "중식",
+        "sequencePriority": 40,
+        "createdAt": new Date(),
+        "updatedAt": new Date()
+    },
+    {
+        "name": "분식",
+        "sequencePriority": 50,
+        "createdAt": new Date(),
+        "updatedAt": new Date()
+    },
+    {
+        "name": "카페∙디저트",
+        "sequencePriority": 60,
+        "createdAt": new Date(),
+        "updatedAt": new Date()
+    },
+    {
+        "name": "아시안",
+        "sequencePriority": 70,
+        "createdAt": new Date(),
+        "updatedAt": new Date()
+    },
+    {
+        "name": "패스트푸드",
+        "sequencePriority": 80,
+        "createdAt": new Date(),
+        "updatedAt": new Date()
+    },
+    {
+        "name": "도시락",
+        "sequencePriority": 90,
+        "createdAt": new Date(),
+        "updatedAt": new Date()
+    },
+    {
+        "name": "간식 (ex. 닭꼬치, 문어빵 등)",
+        "sequencePriority": 100,
+        "createdAt": new Date(),
+        "updatedAt": new Date()
+    },
+    {
+        "name": "기타",
+        "sequencePriority": 110,
+        "createdAt": new Date(),
+        "updatedAt": new Date()
+    }
+])
