acdfcdfe10d8f0b18ee6520b4aca60e234bd7609
seungh0 <will.seungho@gmail.com>
2022-03-14 22:32:51 +0900

    [TD-667] 클라이언트의 IP를 가져올 때, X_forwarded_for 변조 공격 방지

--- threedollar-common/src/main/java/com/depromeet/threedollar/common/utils/ClientIpUtils.java
+++ threedollar-common/src/main/java/com/depromeet/threedollar/common/utils/ClientIpUtils.java
@@ -1,9 +1,14 @@
 package com.depromeet.threedollar.common.utils;
 
+import com.depromeet.threedollar.common.exception.model.InternalServerException;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ClientIpUtils {
@@ -12,10 +17,19 @@ public class ClientIpUtils {
      * 실제 클라이언트의 IP를 가져온다.
      */
     public static String getClientIp(String remoteAddress, @Nullable String forwarded) {
-        if (forwarded == null || forwarded.isBlank()) {
+        if (isPublicIp(remoteAddress) || StringUtils.isBlank(forwarded)) {
             return remoteAddress;
         }
         return extractClientIP(forwarded);
+    }
+
+    private static boolean isPublicIp(String remoteAddress) {
+        try {
+            InetAddress ipAddress = InetAddress.getByName(remoteAddress);
+            return !ipAddress.isSiteLocalAddress();
+        } catch (UnknownHostException e) {
+            throw new InternalServerException(String.format("알 수 없는 remote Address (%s) 입니다", remoteAddress));
+        }
     }
 
     private static String extractClientIP(@NotNull String forwarded) {
