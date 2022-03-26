8fc33368d0acf8b4364522dc5a7292ffc35ab98c
seungh0 <will.seungho@gmail.com>
2022-03-27 04:11:14 +0900

    [TD-688] kotest 마이그레이션

--- threedollar-domain-mongo/src/test/kotlin/com/depromeet/threedollar/domain/mongo/boss/domain/registration/RegistrationTest.kt
+++ threedollar-domain-mongo/src/test/kotlin/com/depromeet/threedollar/domain/mongo/boss/domain/registration/RegistrationTest.kt
@@ -4,69 +4,64 @@ import com.depromeet.threedollar.domain.mongo.boss.domain.account.BossAccountSoc
 import com.depromeet.threedollar.domain.mongo.boss.domain.account.BossAccountSocialType
 import com.depromeet.threedollar.domain.mongo.common.domain.BusinessNumber
 import com.depromeet.threedollar.domain.mongo.common.domain.ContactsNumber
-import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Assertions.assertAll
-import org.junit.jupiter.api.Test
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
 
-internal class RegistrationTest {
+internal class RegistrationTest : FunSpec({
 
-    @Test
-    fun `가입신청을 바탕으로 BossAccout를 생성한다`() {
-        // given
-        val socialId = "social-id"
-        val socialType = BossAccountSocialType.NAVER
-        val bossName = "가삼"
-        val businessNumber = "010-12-12345"
+    context("가입 신청") {
+        test("가입 신청 정보를 바탕으로 BossAccount를 생성한다") {
+            // given
+            val socialId = "social-id"
+            val socialType = BossAccountSocialType.NAVER
+            val bossName = "가삼"
+            val businessNumber = "010-12-12345"
 
-        val registration = RegistrationCreator.create(
-            socialId = socialId,
-            socialType = socialType,
-            bossName = bossName,
-            businessNumber = businessNumber
-        )
-        registration.id = "registrationId"
+            val registration = RegistrationCreator.create(
+                socialId = socialId,
+                socialType = socialType,
+                bossName = bossName,
+                businessNumber = businessNumber
+            )
+            registration.id = "registrationId"
 
-        // when
-        val bossAccount = registration.toBossAccount()
+            // when
+            val bossAccount = registration.toBossAccount()
 
-        // then
-        assertAll({
-            bossAccount.let {
-                assertThat(it.name).isEqualTo(bossName)
-                assertThat(it.socialInfo).isEqualTo(BossAccountSocialInfo.of(socialId, socialType))
-                assertThat(it.businessNumber).isEqualTo(BusinessNumber.of(businessNumber))
+            // then
+            bossAccount.also {
+                it.name shouldBe bossName
+                it.socialInfo shouldBe BossAccountSocialInfo.of(socialId, socialType)
+                it.businessNumber shouldBe BusinessNumber.of(businessNumber)
             }
-        })
+        }
+
+        test("가입 신청 정보를 바탕으로 BossStore를 생성한다") {
+            // given
+            val bossId = "bossId"
+            val storeName = "행잉"
+            val contactsNumber = "010-1234-1234"
+            val categoriesIds = setOf("한식id", "중식id")
+
+            val registration = RegistrationCreator.create(
+                socialId = "social-id",
+                socialType = BossAccountSocialType.NAVER,
+                storeName = storeName,
+                categoriesIds = categoriesIds,
+                contactsNumber = contactsNumber,
+            )
+            registration.id = "registrationId"
+
+            // when
+            val bossStore = registration.toBossStore(bossId)
+
+            // then
+            bossStore.also {
+                it.name shouldBe storeName
+                it.contactsNumber shouldBe ContactsNumber.of(contactsNumber)
+                it.categoriesIds shouldBe categoriesIds
+            }
+        }
     }
 
-    @Test
-    fun `가입신청을 바탕으로 BossStore를 생성한다`() {
-        // given
-        val bossId = "bossId"
-        val storeName = "행잉"
-        val contactsNumber = "010-1234-1234"
-        val categoriesIds = setOf("한식id", "중식id")
-
-        val registration = RegistrationCreator.create(
-            socialId = "social-id",
-            socialType = BossAccountSocialType.NAVER,
-            storeName = storeName,
-            categoriesIds = categoriesIds,
-            contactsNumber = contactsNumber,
-        )
-        registration.id = "registrationId"
-
-        // when
-        val bossStore = registration.toBossStore(bossId)
-
-        // then
-        assertAll({
-            bossStore.let {
-                assertThat(it.name).isEqualTo(storeName)
-                assertThat(it.contactsNumber).isEqualTo(ContactsNumber.of(contactsNumber))
-                assertThat(it.categoriesIds).isEqualTo(categoriesIds)
-            }
-        })
-    }
-
-}
+})
--- threedollar-domain-mongo/src/test/kotlin/com/depromeet/threedollar/domain/mongo/common/domain/TimeIntervalTest.kt
+++ threedollar-domain-mongo/src/test/kotlin/com/depromeet/threedollar/domain/mongo/common/domain/TimeIntervalTest.kt
@@ -1,31 +1,33 @@
 package com.depromeet.threedollar.domain.mongo.common.domain
 
 import com.depromeet.threedollar.common.exception.model.InvalidException
-import org.assertj.core.api.Assertions.assertThatThrownBy
-import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertDoesNotThrow
+import io.kotest.assertions.throwables.shouldNotThrowAny
+import io.kotest.assertions.throwables.shouldThrowExactly
+import io.kotest.core.spec.style.FunSpec
 import java.time.LocalTime
 
-internal class TimeIntervalTest {
+internal class TimeIntervalTest : FunSpec({
 
-    @Test
-    fun `시작시간이 종료시간 이전인경우 에러가 발생하지 않는다`() {
+    test("시작 시간이 종료 시간보다 이전인 경우, 에러가 발생하지 않는다") {
         // given
         val startTime = LocalTime.of(8, 0)
         val endTime = LocalTime.of(8, 1)
 
         // when & then
-        assertDoesNotThrow { TimeInterval(startTime, endTime) }
+        shouldNotThrowAny {
+            TimeInterval(startTime, endTime)
+        }
     }
 
-    @Test
-    fun 시작시간이_종료시간보다_느릴경우_INVALID_EXCEPTION() {
+    test("시작 시간이, 종료시간 보다 이후인경우 Invalid Exception") {
         // given
         val startTime = LocalTime.of(8, 0)
         val endTime = LocalTime.of(7, 59)
 
         // when & then
-        assertThatThrownBy { TimeInterval(startTime, endTime) }.isInstanceOf(InvalidException::class.java)
+        shouldThrowExactly<InvalidException> {
+            TimeInterval(startTime, endTime)
+        }
     }
 
-}
+})
