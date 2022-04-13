c1ad11239e6c5dc7c9c02297f8275954cd0a0cb5
seungh0 <will.seungho@gmail.com>
2022-04-14 07:16:12 +0900

    [TD-722] spring security 설정 수정

--- threedollar-api-boss/src/main/kotlin/com/depromeet/threedollar/api/boss/config/security/WebSecurityConfig.kt
+++ threedollar-api-boss/src/main/kotlin/com/depromeet/threedollar/api/boss/config/security/WebSecurityConfig.kt
@@ -2,7 +2,6 @@ package com.depromeet.threedollar.api.boss.config.security
 
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.config.annotation.web.builders.WebSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.http.SessionCreationPolicy
@@ -11,20 +10,12 @@ import org.springframework.security.config.http.SessionCreationPolicy
 @Configuration
 class WebSecurityConfig : WebSecurityConfigurerAdapter() {
 
-    override fun configure(web: WebSecurity) {
-        web.ignoring().antMatchers(
-            "/h2-console/**",
-            "/swagger-resources/**",
-            "swagger-ui/**",
-            "/v2/api-docs",
-            "/webjars/**"
-        )
-    }
-
     override fun configure(http: HttpSecurity) {
         http.authorizeRequests()
                 .antMatchers("/monitoring/**").authenticated()
-                    .anyRequest().permitAll()
+                .anyRequest().permitAll()
+            .and()
+                .requestMatchers {   it.antMatchers(   "/h2-console/**") }.headers().frameOptions().sameOrigin()
             .and()
                 .httpBasic()
             .and()
--- threedollar-api-user/src/main/java/com/depromeet/threedollar/api/user/config/security/WebSecurityConfig.java
+++ threedollar-api-user/src/main/java/com/depromeet/threedollar/api/user/config/security/WebSecurityConfig.java
@@ -2,7 +2,6 @@ package com.depromeet.threedollar.api.user.config.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -12,21 +11,12 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
-    public void configure(WebSecurity web) {
-        web.ignoring().antMatchers(
-                "/h2-console/**",
-                "/swagger-resources/**",
-                "swagger-ui/**",
-                "/v2/api-docs",
-                "/webjars/**"
-            );
-    }
-
-    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
             .antMatchers("/monitoring/**").authenticated()
                 .anyRequest().permitAll()
+            .and()
+                .requestMatchers((matcher) -> matcher.antMatchers("/h2-console/**")).headers().frameOptions().sameOrigin()
             .and()
                 .httpBasic()
             .and()
