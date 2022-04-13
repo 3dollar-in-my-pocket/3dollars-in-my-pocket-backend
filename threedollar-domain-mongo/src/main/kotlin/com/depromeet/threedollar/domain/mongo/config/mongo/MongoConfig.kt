67ce7373e7728c30240ffbc3371fd3f8c918bc68
seungh0 <will.seungho@gmail.com>
2022-04-13 23:16:15 +0900

    [TD-721] MongoDB set ReadPreference secondaryPreferred

--- threedollar-domain-mongo/src/main/kotlin/com/depromeet/threedollar/domain/mongo/config/mongo/MongoConfig.kt
+++ threedollar-domain-mongo/src/main/kotlin/com/depromeet/threedollar/domain/mongo/config/mongo/MongoConfig.kt
@@ -1,24 +1,32 @@
 package com.depromeet.threedollar.domain.mongo.config.mongo
 
 import com.depromeet.threedollar.domain.mongo.ThreeDollarDomainMongoRoot
+import com.mongodb.ReadPreference
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.mongodb.MongoDatabaseFactory
 import org.springframework.data.mongodb.config.EnableMongoAuditing
+import org.springframework.data.mongodb.core.MongoTemplate
 import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver
 import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories
-
 import org.springframework.data.mongodb.core.convert.MappingMongoConverter
 import org.springframework.data.mongodb.core.mapping.MongoMappingContext
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories
 
 @EnableMongoAuditing
-@Configuration
 @EnableMongoRepositories(basePackageClasses = [ThreeDollarDomainMongoRoot::class])
+@Configuration
 class MongoConfig(
     private val mongoDbFactory: MongoDatabaseFactory,
     private val mongoMappingContext: MongoMappingContext
 ) {
+
+    @Bean
+    fun mongoTemplate(mongoDatabaseFactory: MongoDatabaseFactory): MongoTemplate {
+        val mongoTemp = MongoTemplate(mongoDatabaseFactory, mappingMongoConverter())
+        mongoTemp.setReadPreference(ReadPreference.secondaryPreferred())
+        return mongoTemp
+    }
 
     @Bean
     fun mappingMongoConverter(): MappingMongoConverter {
@@ -29,3 +37,4 @@ class MongoConfig(
     }
 
 }
+
