eb2612300f835c61779a9aa6d4912494cbb4d5d5
seungh0 <will.seungho@gmail.com>
2022-05-03 03:21:06 +0900

    [TD-769] api-core Root Kotlin 전환

---
+++ threedollar-api-core/src/main/java/com/depromeet/threedollar/api/core/ThreeDollarApiCoreRoot.kt
@@ -0,0 +1,12 @@
+package com.depromeet.threedollar.api.core
+
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
+import com.depromeet.threedollar.domain.rds.ThreeDollarDomainRdsRoot
+
+@ComponentScan(basePackageClasses = [
+    ThreeDollarApiCoreRoot::class,
+    ThreeDollarDomainRdsRoot::class
+])
+@Configuration
+class ThreeDollarApiCoreRoot
