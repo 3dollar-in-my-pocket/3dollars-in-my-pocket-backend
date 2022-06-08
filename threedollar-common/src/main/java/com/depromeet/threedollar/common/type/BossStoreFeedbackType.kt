ede5205b603e5706a668cd39f515019859cf226a
seungh0 <will.seungho@gmail.com>
2022-06-09 01:10:35 +0900

    [TD-872] 사장님 가게 피드백 타입 변경

--- threedollar-common/src/main/java/com/depromeet/threedollar/common/type/BossStoreFeedbackType.kt
+++ threedollar-common/src/main/java/com/depromeet/threedollar/common/type/BossStoreFeedbackType.kt
@@ -7,14 +7,14 @@ enum class BossStoreFeedbackType(
     val emoji: String,
 ) : EnumModel {
 
-    HANDS_ARE_FAST("손이 빠르세요", "\uD83D\uDE0B"),
-    FOOD_IS_DELICIOUS("음식이 맛있어요", "\uD83D\uDE0B"),
+    HANDS_ARE_FAST("손이 빠르세요", "👐"),
+    FOOD_IS_DELICIOUS("음식이 맛있어요", "😋"),
     HYGIENE_IS_CLEAN("위생이 청결해요", "✨"),
-    BOSS_IS_KIND("사장님이 친절해요", "\uD83D\uDE4F"),
-    CAN_PAY_BY_CARD("카드 결제 가능해요", "\uD83D\uDCB3"),
+    BOSS_IS_KIND("사장님이 친절해요", "🙏"),
+    CAN_PAY_BY_CARD("카드 결제 가능해요", "💳"),
     GOOD_VALUE_FOR_MONEY("가성비가 좋아요", "💰"),
-    GOOD_TO_EAT_IN_ONE_BITE("한입에 먹기 좋아요", "\uD83D\uDC4C"),
-    GOT_A_BONUS("덤도 주셨어요", "\uD83D\uDC4D"),
+    GOOD_TO_EAT_IN_ONE_BITE("한입에 먹기 좋아요", "👌"),
+    GOT_A_BONUS("덤도 주셨어요", "👍"),
     ;
 
     override fun getKey(): String {
