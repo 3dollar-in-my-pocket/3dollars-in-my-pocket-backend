ae2de09d9bf1e7dfeaaa82d8e0b46ddd8d9ea39c
seungh0 <will.seungho@gmail.com>
2022-07-02 15:28:34 +0900

    [TD-910] BenchMark StopWatch 사용하도록 수정

--- threedollar-api-core/src/main/java/com/depromeet/threedollar/api/core/config/benchmark/BenchMarkAspect.java
+++ threedollar-api-core/src/main/java/com/depromeet/threedollar/api/core/config/benchmark/BenchMarkAspect.java
@@ -1,11 +1,10 @@
 package com.depromeet.threedollar.api.core.config.benchmark;
 
-import java.util.concurrent.TimeUnit;
-
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -16,12 +15,13 @@ public class BenchMarkAspect {
 
     @Around("@annotation(com.depromeet.threedollar.api.core.config.benchmark.BenchMark)")
     public Object calculatePerformance(ProceedingJoinPoint joinPoint) throws Throwable {
-        long startTime = System.nanoTime();
+        StopWatch stopWatch = new StopWatch();
         try {
+            stopWatch.start();
             return joinPoint.proceed(joinPoint.getArgs());
         } finally {
-            long endTime = System.nanoTime();
-            log.info("Performance: {}s", (TimeUnit.NANOSECONDS.toMillis(endTime - startTime) / 1000.0));
+            stopWatch.stop();
+            log.info("Performance: {}s", (stopWatch.getTotalTimeMillis()));
         }
     }
 
