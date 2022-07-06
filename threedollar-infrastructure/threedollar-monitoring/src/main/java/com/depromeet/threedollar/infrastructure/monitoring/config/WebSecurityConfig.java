6557cfab9dbe31fa0ee5e5a31f3219dca24329ed
seungh0 <will.seungho@gmail.com>
2022-07-07 04:16:43 +0900

    [TD-908] monitoring 모듈화

--- threedollar-infrastructure/threedollar-monitoring/src/main/java/com/depromeet/threedollar/infrastructure/monitoring/config/WebSecurityConfig.java
+++ threedollar-infrastructure/threedollar-monitoring/src/main/java/com/depromeet/threedollar/infrastructure/monitoring/config/WebSecurityConfig.java
@@ -1,4 +1,4 @@
-package com.depromeet.threedollar.api.core.config.security;
+package com.depromeet.threedollar.infrastructure.monitoring.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
