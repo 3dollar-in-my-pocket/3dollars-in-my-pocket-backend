7b7b954c779fb604bc09ab82d513128d8ae2c389
seungh0 <will.seungho@gmail.com>
2022-07-19 20:26:19 +0900

    [TD-937] 유저 메뉴 카테고리 응답 파라미터 수정 category -> categoryId (category Deprecated)

--- threedollar-api-core/src/main/java/com/depromeet/threedollar/api/core/mapper/userservice/menu/MenuCategoryResponse.kt
+++ threedollar-api-core/src/main/java/com/depromeet/threedollar/api/core/mapper/userservice/menu/MenuCategoryResponse.kt
@@ -4,7 +4,9 @@ import com.depromeet.threedollar.common.type.UserMenuCategoryType
 import com.fasterxml.jackson.annotation.JsonProperty
 
 data class MenuCategoryResponse(
+    @Deprecated(message = "v3.0.6부터 categoryId로 변경")
     val category: UserMenuCategoryType,
+    val categoryId: UserMenuCategoryType,
     val name: String,
     val description: String,
     @get:JsonProperty("isNew")
@@ -16,6 +18,7 @@ data class MenuCategoryResponse(
         fun of(category: UserMenuCategoryType): MenuCategoryResponse {
             return MenuCategoryResponse(
                 category = category,
+                categoryId = category,
                 name = category.categoryName,
                 description = category.description,
                 isNew = category.isNew
