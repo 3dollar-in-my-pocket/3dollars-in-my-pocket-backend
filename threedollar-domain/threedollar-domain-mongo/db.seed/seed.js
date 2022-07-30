59f36c8c2a1432f93547badf6f65e89ce3dece1e
seungh0 <will.seungho@gmail.com>
2022-07-30 12:24:00 +0900

    [TD-959] 사장님 가게 카테고리 시드 데이터 수정

--- threedollar-domain/threedollar-domain-mongo/db.seed/seed.js
+++ threedollar-domain/threedollar-domain-mongo/db.seed/seed.js
@@ -2,66 +2,77 @@
 db.boss_store_category_v1.insertMany([
     {
         "name": "한식",
+        "imageUrl": "https://storage.threedollars.co.kr/menu/icon_menu_60pt%3Dbab.png",
         "sequencePriority": 10,
         "createdAt": new Date(),
         "updatedAt": new Date()
     },
     {
         "name": "양식",
+        "imageUrl": "https://storage.threedollars.co.kr/menu/icon_menu_60pt%3Dpizza.png",
         "sequencePriority": 20,
         "createdAt": new Date(),
         "updatedAt": new Date()
     },
     {
         "name": "일식",
+        "imageUrl": "https://storage.threedollars.co.kr/menu/icon_menu_60pt%3Dsushi.png",
         "sequencePriority": 30,
         "createdAt": new Date(),
         "updatedAt": new Date()
     },
     {
         "name": "중식",
+        "imageUrl": "https://storage.threedollars.co.kr/menu/icon_menu_60pt%3DJajangmyeon.png",
         "sequencePriority": 40,
         "createdAt": new Date(),
         "updatedAt": new Date()
     },
     {
         "name": "분식",
+        "imageUrl": "https://storage.threedollars.co.kr/menu/icon_menu_60pt%3Dtteogbokki%2C+color%3Dblack.png",
         "sequencePriority": 50,
         "createdAt": new Date(),
         "updatedAt": new Date()
     },
     {
         "name": "카페∙디저트",
+        "imageUrl": "https://storage.threedollars.co.kr/menu/icon_menu_60pt%3D_cake.png",
         "sequencePriority": 60,
         "createdAt": new Date(),
         "updatedAt": new Date()
     },
     {
         "name": "아시안",
+        "imageUrl": "https://storage.threedollars.co.kr/menu/icon_menu_60pt%3D_curry.png",
         "sequencePriority": 70,
         "createdAt": new Date(),
         "updatedAt": new Date()
     },
     {
         "name": "패스트푸드",
+        "imageUrl": "https://storage.threedollars.co.kr/menu/icon_menu_60pt%3Dhamburger.png",
         "sequencePriority": 80,
         "createdAt": new Date(),
         "updatedAt": new Date()
     },
     {
         "name": "도시락",
+        "imageUrl": "https://storage.threedollars.co.kr/menu/icon_menu_60pt%3Ddosirak.png",
         "sequencePriority": 90,
         "createdAt": new Date(),
         "updatedAt": new Date()
     },
     {
         "name": "간식 (ex. 닭꼬치, 문어빵 등)",
+        "imageUrl": "https://storage.threedollars.co.kr/menu/icon_menu_60pt%3Ddalgkkochi%2C+color%3Dblack.png",
         "sequencePriority": 100,
         "createdAt": new Date(),
         "updatedAt": new Date()
     },
     {
         "name": "기타",
+        "imageUrl": "https://storage.threedollars.co.kr/menu/icon_menu_60pt%3D_taco.png",
         "sequencePriority": 110,
         "createdAt": new Date(),
         "updatedAt": new Date()
